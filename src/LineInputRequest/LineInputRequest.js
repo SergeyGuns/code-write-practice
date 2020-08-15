921d5627d2f82a00849dc74d67fb5d4876a9f6df
Sergey <mcheguevara2@gmail.com>
2020-08-16 00:24:12 +0300

    fix

---
+++ src/LineInputRequest/LineInputRequest.js
@@ -0,0 +1,87 @@
+import React from "react";
+import "./LineInputRequest.scss";
+import cx from "classnames";
+import {
+  useGlobalKeyPress,
+  useGlobalKeyDown,
+} from "./LineInputRequestHandlers";
+
+export function Letter({ letter, isTyped, isMissLetter, isSpace, key }) {
+  return (
+    <span
+      key={key}
+      className={cx("Letter", { isMissLetter, isTyped, isSpace })}
+    >
+      {isSpace ? "•" : letter}
+    </span>
+  );
+}
+
+export function NewLineIndicator() {
+  return <span className="NewLineIndicator">↵ Enter</span>;
+}
+
+export function isEqualLine(a, b) {
+  if (typeof a === "string" && typeof b === "string") {
+    return a === b;
+  }
+}
+
+export function Line({ requireLine, typedLine }) {
+  console.log({ requireLine, typedLine });
+  const line = requireLine.split("").map((letter, i) => ({
+    letter,
+    isSpace: letter === " ",
+    isTyped: i < typedLine.length,
+    isMissLetter: ((a, b) => b !== undefined && a !== b)(letter, typedLine[i]),
+    typedLetter: typedLine[i],
+  }));
+
+  return (
+    <pre className="LineInputRequest">
+      {line.map(
+        ({ letter, isTyped, isMissLetter, typedLetter, isSpace }, i) => (
+          <Letter
+            isSpace={isSpace}
+            key={i + letter}
+            letter={isMissLetter ? typedLetter : letter}
+            isTyped={isTyped}
+            isMissLetter={isMissLetter}
+          />
+        )
+      )}
+      {isEqualLine(requireLine, typedLine) && <NewLineIndicator />}
+    </pre>
+  );
+}
+
+function LineInputRequest(props) {
+  const [requireLine, setRequireLine] = React.useState("abc dfe");
+  const [typedLine, setTypedLine] = React.useState("");
+
+  const handleInputAddLetter = ({ key }) => {
+    setTypedLine(typedLine + key);
+  };
+  const popLine = (line) =>
+    line.length ? line.substr(0, line.length - 1) : "";
+
+  const handleBackspacePress = (callback) => ({ keyCode }) => {
+    const BACKSPACE_KEY_CODE = 8;
+    if (keyCode === BACKSPACE_KEY_CODE) callback();
+  };
+  const handleEnterPress = (callback) => ({ keyCode }) => {
+    const ENTER_KEY_CODE = 13;
+    if (keyCode === ENTER_KEY_CODE) callback();
+  };
+  useGlobalKeyPress(handleInputAddLetter);
+  useGlobalKeyDown(
+    handleBackspacePress(() => setTypedLine(popLine(typedLine)))
+  );
+  return (
+    <div>
+      <Line requireLine={requireLine} typedLine={typedLine} />
+    </div>
+  );
+}
+
+export default LineInputRequest;
---
+++ src/LineInputRequest/LineInputRequestHandlers.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+export const useWindowEvent = (event, callback) => {
+  useEffect(() => {
+    window.addEventListener(event, callback);
+    return () => window.removeEventListener(event, callback);
+  }, [event, callback]);
+};
+
+export const useGlobalKeyPress = (callback) => {
+  return useWindowEvent("keypress", callback);
+};
+
+export const useGlobalKeyDown = (callback) => {
+  return useWindowEvent("keydown", callback);
+};
